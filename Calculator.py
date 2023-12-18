2a4c0a1a78d999afa2407713e58064b47a1dd28c
Alex <114363651+alex-149@users.noreply.github.com>
2023-12-19 09:11:00 +1300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,87 +1,37 @@
 while True:
-    def Calcluate(x, y):
-        if '+' in math_operations:
-            return x + y
-        
-        if '-' in math_operations:
-            return x - y
-        
-        if '*' in math_operations:
-            return x * y
-        
-        if '/' in math_operations:
-            return x / y
-        if '^' in math_operations:
-            return x ** y
-    
-    z = str(input("Math Equation: "))
-    
-    math_operations = []    
-            
-    zs = z.split()
-    
-    for word in zs:
-        if word in ['+', '-', '*', '/', '^']:
-            math_operations.append(word)
-                    
+        def Calcluate(x, y):
             if '+' in math_operations:
+                return x + y
+            
+            if '-' in math_operations:
+                return x - y
+            
+            if '*' in math_operations:
+                return x * y
+            
+            if '/' in math_operations:
+                return x / y
+            if '^' in math_operations:
+                return x ** y
+
+        z = str(input("Math Equation: "))
+
+        math_operations = []    
+            
+        zs = z.split()
+
+        for word in zs:
+            if word in ['+', '-', '*', '/', '^']:
+                math_operations.append(word)
+                        
+        if '+' in math_operations or '-' in math_operations or '*' in math_operations or '/' in math_operations or '^' in math_operations:
+                    
+            x = zs[0]
+            x = float(x)
                 
-                x = zs[0]
-                x = float(x)
+            y = zs[2]
+            y = float(y)
                 
-                y = zs[2]
-                y = float(y)
-                
-                print(Calcluate(x, y))
-                
-            elif '-' in math_operations:
-                
-                x = zs[0]
-                x = float(x)
-    
-                y = zs[2]
-                y = float(y)
-                
-                result = [Calcluate(x,y)]
-                
-                print(Calcluate(x, y))
-                
-            elif '*' in math_operations:
-                
-                x = zs[0]
-                x = float(x)
-                
-                y = zs[2]
-                y = float(y)
-                
-                result = [Calcluate(x,y)]
-                
-                print(Calcluate(x, y))
-                
-            elif '/' in math_operations:
-                
-                x = zs[0]
-                x = float(x)
-                
-                y = zs[2]
-                y = float(y)
-                
-                if x or y == 0:
-                    print("Error Cannot Divide by Zero")
-                    break
-                
-                result = [Calcluate(x,y)]
-                
-                print(Calcluate(x, y))
-                
-            elif '^' in math_operations:
-                
-                x = zs[0]
-                x = float(x)
-                
-                y = zs[2]
-                y = float(y)
-                
-                result = [Calcluate(x,y)]
-                
-                print(Calcluate(x, y))
+            print(Calcluate(x, y))
+        else: 
+            print("Must be a valid math equation.")
